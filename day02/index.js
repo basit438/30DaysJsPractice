4961d77d7fc9876fdced86359172193952505cd6
Basit <basitmanzoor438@gmail.com>
2024-07-18 08:43:37 +0530

    upload day02 of js

---
+++ day02/index.js
@@ -0,0 +1,69 @@
+// Activity 1
+
+// task 1
+
+let a = 7+6;
+console.log(a);
+
+// task 2
+let b = 7-6;
+console.log(b);
+
+// task3
+let c = 7*6;
+console.log(c);
+
+// task4
+let d = 7/6;
+console.log(d)
+
+// task4
+let e = 7%6;
+console.log(e)
+
+
+// activity2
+
+// task6
+
+a+=3;
+console.log(a); //added 3 to a//
+
+// task7
+
+c-=5;
+console.log(c); //subtracted 5 from c using assignment operator
+
+// Activity 3
+
+// task8
+
+let isGreater =a>b;
+console.log(isGreater);
+
+// task9
+
+isGreater =a>=b;
+console.log(isGreater);
+
+// task 10
+
+isGreater =a===b;
+console.log(isGreater);
+
+// Activity4
+
+if(a>b && b<c){
+    console.log("b is lesser than both a and b")
+}
+if(a>b || b<c){
+    console.log("b is lesser than either a or b")
+}
+if(!a<b){
+    console.log("a is  greater")
+}
+
+// Activity6
+
+(a>b) ? console.log("a is greater")  : console.log("a is not greater");  //checking the condition using ternary opertaor
+
